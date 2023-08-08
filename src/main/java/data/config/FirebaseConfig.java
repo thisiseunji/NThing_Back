d7163c4b6c255c2d81f16f05a188aec25b7cb4e6
kkookkss <pre0919@gmail.com>
2023-08-09 06:27:27 +0900

    ♻️ Refactor: FirebaseConfig keyFile 수정

--- src/main/java/data/config/FirebaseConfig.java
+++ src/main/java/data/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package data.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,14 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${spring.firebase.credentials.key-file}")
+    private String keyFile;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
 
-        FileInputStream serviceAccount = new FileInputStream("/Users/kkookkss/dev/nthing-firebase.json");
+
+        FileInputStream serviceAccount = new FileInputStream(keyFile);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
