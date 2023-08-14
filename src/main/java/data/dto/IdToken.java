45e719df611f80c87c9b30c7a01e58c529aa0d3c
kkookkss <pre0919@gmail.com>
2023-08-14 13:47:53 +0900

    ♻️ Refactor: id token 요청 키값 수정 (idToken -> id_token)

--- src/main/java/data/dto/IdToken.java
+++ src/main/java/data/dto/IdToken.java
@@ -1,8 +1,10 @@
 package data.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class IdToken {
+    @JsonProperty("id_token")
     private String idToken;
 }
