af6bf730f67950cf241593b09d317fd7192e3b71
kkookkss <pre0919@gmail.com>
2023-10-21 23:56:30 +0900

    ♻️ Refactor: JwtToken dto 생성자 어노테이션 추가

--- src/main/java/data/dto/JwtToken.java
+++ src/main/java/data/dto/JwtToken.java
@@ -1,10 +1,12 @@
 package data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class JwtToken {
     private String token;
 }
