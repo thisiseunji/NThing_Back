7d014ff37cb6bc8ef80b188a411c84a2a9bdd04e
kkookkss <pre0919@gmail.com>
2024-01-06 23:52:51 +0900

    :recycle: Refactor: User 관련 api 응답 형태 및 로직 수정 - ResponseEntity에 ApiReponse 형태로 응답되도록 적용 - token 파싱 후 해당 userId에 대해 db에 있는지 체크하는 로직을 컨트롤러에서 서비스로 옮김

--- src/main/java/data/controller/UserController.java
+++ src/main/java/data/controller/UserController.java
@@ -1,18 +1,17 @@
 package data.controller;
 
-import data.constants.ErrorCode;
-import data.dto.ApiResponseEntity;
+import data.dto.ApiResponse;
 import data.dto.MessageTokenDto;
 import data.dto.PurchaseDto;
 import data.dto.UserDto;
-import data.exception.UserNotFoundException;
 import data.service.UserService;
 import data.service.LikeService;
 import data.util.JwtProvider;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,33 +19,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
-@Slf4j
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
     private final LikeService likeService;
     private final JwtProvider jwtProvider;
 
-    @Autowired
-    public UserController(UserService userService, LikeService likeService, JwtProvider jwtProvider) {
-        this.userService = userService;
-        this.likeService = likeService;
-        this.jwtProvider = jwtProvider;
-    }
-
     @GetMapping("")
-    public ApiResponseEntity<UserDto> findById(@RequestHeader("Authorization") String token) {
-        UserDto user = userService.findById(token);
-        if (user == null) {
-            throw new UserNotFoundException("User not found for the provided token.", ErrorCode.USER_NOT_FOUND);
-        }
-        return ApiResponseEntity.ok(user);
+    public ResponseEntity<ApiResponse<UserDto>> findById(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(ApiResponse.ok(userService.findById(token)));
     }
 
     @GetMapping("/like")
-    public ApiResponseEntity<List<PurchaseDto.Summary>> findLikedPurchasesByUserId(
+    public ResponseEntity<ApiResponse<List<PurchaseDto.Summary>>> findLikedPurchasesByUserId(
             @RequestHeader("Authorization") String token,
             String search_keyword,
             String sort
@@ -56,14 +45,14 @@ public class UserController {
         map.put("search_keyword", search_keyword);
         map.put("sort", sort);
         map.put("user_id", userId);
-        List<PurchaseDto.Summary> likedPurchases = likeService.findLikedPurchasesByUserId(map);
-        return ApiResponseEntity.ok(likedPurchases);
+
+        return ResponseEntity.ok(ApiResponse.ok(likeService.findLikedPurchasesByUserId(map)));
     }
 
     @DeleteMapping("")
-    public ApiResponseEntity<?> deleteUser(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<ApiResponse<?>> deleteUser(@RequestHeader("Authorization") String token) {
         userService.deleteUser(token);
-        return ApiResponseEntity.noContent();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.noContent());
     }
 
     // 리프레시 토큰 검증
