8dfe419db128ea6f40bb6048111cba3355a58e28
kkookkss <pre0919@gmail.com>
2024-01-06 23:45:12 +0900

    :recycle: Refactor: ApiResponse 메서드 추가 - 각 crud 작업에 사용될 메서드 설정

--- src/main/java/data/dto/ApiResponse.java
+++ src/main/java/data/dto/ApiResponse.java
@@ -1,5 +1,6 @@
 package data.dto;
 
+import data.constants.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -12,4 +13,25 @@ public class ApiResponse<T> {
     private final String message;
     private final int status;
     private final String code;
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(data, null, 200, null);
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(data, null, 201, null);
+    }
+
+    public static ApiResponse<?> created() {
+        return new ApiResponse<>(null, null, 201, null);
+    }
+
+    public static ApiResponse<?> noContent() {
+        return new ApiResponse<>(null, null, 204, null);
+    }
+
+    public static ApiResponse<?> error(ErrorCode errorCode) {
+        ErrorResponse response = new ErrorResponse(errorCode);
+        return new ApiResponse<>(null, response.getMessage(), response.getStatus(), response.getCode());
+    }
 }
