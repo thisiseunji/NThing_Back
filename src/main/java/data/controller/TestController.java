3da79e3dac934ffccafd1b332946092143c0b778
kkookkss <pre0919@gmail.com>
2024-01-07 17:36:02 +0900

    :recycle: Refactor: swagger 적용에 따른 api 설명 추가 - CategoryController - TestController - ErrorResponse

--- src/main/java/data/controller/TestController.java
+++ src/main/java/data/controller/TestController.java
@@ -1,28 +1,42 @@
 package data.controller;
 
-import data.dto.ApiResponse;
+import data.dto.ApiResult;
+import data.dto.ErrorResponse;
 import data.dto.MessageTokenDto;
 import data.service.UserService;
 import data.util.JwtProvider;
-import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "test", description = "테스트용 API")
 @RestController
 @RequiredArgsConstructor
 public class TestController {
     private final JwtProvider jwtProvider;
     private final UserService userService;
 
+    @Operation(summary = "테스트용 토큰 발급", description = "path variable로 id를 받아서 토큰을 발급합니다.")
+    @Parameter(name = "id", description = "테스트용 토큰 발급을 위한 user_id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "테스트용 토큰 발급 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResult.class))),
+            @ApiResponse(responseCode = "400", description = "테스트용 토큰 발급 실패", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "유저를 찾을 수 없음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping("/test/token/{id}")
-    @ApiOperation(value = "테스트용 토큰 발급", notes = "path variable로 id를 받아서 토큰을 발급합니다.")
-    public ResponseEntity<ApiResponse<MessageTokenDto>> getTestToken(@PathVariable int id) {
+    public ResponseEntity<ApiResult<MessageTokenDto>> getTestToken(@PathVariable int id) {
         String token = jwtProvider.createToken(id);
         userService.findById(token);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(ApiResponse.created(new MessageTokenDto("테스트 토큰 생성", token, null)));
+                .body(ApiResult.created(new MessageTokenDto("테스트 토큰 생성", token, null)));
     }
 }
