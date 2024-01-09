0b78ffd23a291643fd1e605b44c4388c2364ed65
kkookkss <pre0919@gmail.com>
2024-01-09 20:51:21 +0900

    :recycle: Refactor: 구글 로그인 응답 형태 변경

--- src/main/java/data/controller/LoginController.java
+++ src/main/java/data/controller/LoginController.java
@@ -1,11 +1,11 @@
 package data.controller;
 
 import com.google.firebase.auth.FirebaseAuthException;
+import data.dto.ApiResult;
 import data.dto.IdToken;
 import data.dto.MessageTokenDto;
 import data.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +22,11 @@ public class LoginController {
     }
 
     @PostMapping("/login/google")
-    public ResponseEntity<MessageTokenDto> googleLogin(@RequestBody IdToken token) {
+    public ResponseEntity<ApiResult<MessageTokenDto>> googleLogin(@RequestBody IdToken token) {
         try {
-            return new ResponseEntity<>(loginService.googleLogin(token), HttpStatus.OK);
+            return ResponseEntity.ok(ApiResult.ok(loginService.googleLogin(token)));
         } catch (FirebaseAuthException e) {
-            return new ResponseEntity<>(new MessageTokenDto("구글 로그인 실패", null, null), HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
         }
     }
 
