7a1fd76a3c26ecbbe0d29ba576f207abc12e8a5e
thisiseunji <monahk93@gmail.com>
2024-04-11 21:28:16 +0900

    Add : 유저 ID를 이용한 채팅방 조회

--- src/main/java/data/GlobalExceptionHandler.java
+++ src/main/java/data/GlobalExceptionHandler.java
@@ -6,15 +6,10 @@ import data.exception.NullPointerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
