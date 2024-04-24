be2f578dfbeb61348f66fad708b5bd3d71caf5a0
kkookkss <pre0919@gmail.com>
2024-04-24 23:53:21 +0900

    🔨 좋아요 한 거래 목록: 거래글 이미지 값에 url 포함되도록 수정

--- src/main/java/data/service/LikeService.java
+++ src/main/java/data/service/LikeService.java
@@ -3,29 +3,40 @@ package data.service;
 import data.dto.LikeDto;
 import data.dto.PurchaseDto;
 import data.mapper.LikeMapper;
+import data.util.MultiFileUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class LikeService {
 
     private final LikeMapper likeMapper;
-
-    @Autowired
-    public LikeService(LikeMapper likeMapper) {
-        this.likeMapper = likeMapper;
-    }
+    private final MultiFileUtils multiFileUtils;
 
     public void createLike(LikeDto likeDto){
         likeMapper.createLike(likeDto);
     }
 
     public List<PurchaseDto.Summary> findLikedPurchasesByUserId(Map<String, Object> map){
-        return likeMapper.findLikedPurchasesByUserId(map);
+        List<PurchaseDto.Summary> result = likeMapper.findLikedPurchasesByUserId(map);
+        List<PurchaseDto.Summary> generatePurchaseDtoList = new ArrayList<>();
+        for (PurchaseDto.Summary purchaseDto : result) {
+            String image = purchaseDto.getImage();
+            purchaseDto.setImage(image != null
+                    ? multiFileUtils.getDomain() + image
+                    : null
+            );
+            generatePurchaseDtoList.add(purchaseDto);
+        }
+
+        return generatePurchaseDtoList;
     }
 
     public Optional<Integer> findLikeIdByUserIdAndPurchaseId(int userId, int purchaseId) {
